2feae7ba61af79bbe6941354c2aa6b6b5faeeb5f
hth225 <hth225@naver.com>
2018-05-25 17:55:49 +0900

    솔리디티 예제 추가

---
+++ Solidity/README.md
@@ -0,0 +1,11 @@
+# Solidity
+> - 스마트 컨트랙트 구현을 위한 컨트랙트 기반의 고급 프로그래밍 언어
+> - C++, Python, JavaScript의 영향을 받음
+> - EVM(Ethereum Virtual Machine)에서 구동하도록 설계되었음
+
+## 학습
+- [Remix](https://remix.ethereum.org/)를 사용함
+
+### what is Remix?
+> 별도의 서버없이 컴파일러와 런타임 환경을 제공하는 브라우저 기반 IDE
+
---
+++ Solidity/ballot.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.4.16;
+
+/// @title 대표단과 투표
+contract Ballot 
+{
+    //추후에 변수에 사용될 
+    //새로운 복합 유형을 선언
+    //그것은 단일 유권자를 대표할 것
+    struct Voter 
+    {
+        uint weight; //대표단에 의해 누적
+        bool voted; //만약 이 값이 true 라면, 해당 유저는 투표를 완료한 상태
+        address delegate; //투표에 위임된 사람
+        uint vote; //투표된 제안의 인덱스 데이터 값
+
+    }
+    //단일 제안에 대한 유형
+    struct Proposal
+    {
+        bytes32 name; //간단한 명칭 (최대 32바이트)
+        uint voteCount; //누적 투표 수
+    }
+    address public chairperson;
+    //이것은 각각의 가능한 주소에 대해 
+    //'voter' 구조체를 저장하는 상태변수를 선언함
+    mapping(address=>Voter) public voters;
+
+    //동적으로 크기가 지정된 'Proposal' 구조체의 배열
+    Proposal[] public proposals;
+
+    /// 'proposalNames 중 하나를 선택하기 위한 새로운 투표권 생성해야 함
+    // funciton Ballot(bytes32[] proposalNames) public
+    // {
+    //     chairperson = msg.sender;
+    //     voters[chairperson].weight = 1;
+
+    // }
+}
