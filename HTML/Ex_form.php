f35cf5a3a5b4557c5661650cbe67cfc696681b47
hth225 <hth225@naver.com>
2018-06-25 16:23:34 +0900

    form 예제 추가

---
+++ HTML/Ex_form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <?php
+    echo $_REQUEST['nickname'].'님의 직업은'.$_REQUEST['job'].'이시군요!';
+    ?>
+</body>
+</html>
