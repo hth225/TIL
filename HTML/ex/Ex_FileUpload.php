7496fef106afa0286c0f5bbff54ce9f306b6de2d
hth225 <hth225@naver.com>
2018-06-30 23:27:25 +0900

    파일 업로드

--- HTML/ex/Ex_FileUpload.php
+++ HTML/ex/Ex_FileUpload.php
@@ -5,7 +5,10 @@
 </head>
 <body>
     <?php
-    $uploadir = ''
+    $uploadir = '';
+    $uploadfile = $uploadir.basename($_FILES['image']['name']);
+    move_uploaded_file($_FILES['image']['tmp_name'].$uploadfile);
     ?>
+    <img src="<?=$_FILES['image']['name']?>" />
 </body>
 </html>
