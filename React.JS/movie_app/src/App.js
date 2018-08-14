63ae8b56d545fd905d820c87d86ba30b145f747e
hth225 <hth225@naver.com>
2018-08-14 20:59:41 +0900

    Validating props with prop types

--- React.JS/movie_app/src/App.js
+++ React.JS/movie_app/src/App.js
@@ -28,8 +28,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {movies.map(movie => {
-          return <Movie title={movie.title} poster={movie.poster} />
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index}/>
         })}
       </div>
     );
--- React.JS/movie_app/src/Movie.js
+++ React.JS/movie_app/src/Movie.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css'
 
 class Movie extends Component {
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired,
+
+    }
+
     render() {
         return (
             <div>
@@ -13,6 +21,10 @@ class Movie extends Component {
 }
 
 class MoviePoster extends Component {
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
     render() {
         console.log(this.props);
         return(
