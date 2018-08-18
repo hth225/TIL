ab1b84aab214c6dcd9fc903e323f3a53d324e0fe
hth225 <hth225@naver.com>
2018-08-18 21:26:13 +0900

    Smart component, Dumb Component

--- React.JS/movie_app/src/Movie.js
+++ React.JS/movie_app/src/Movie.js
@@ -1,37 +1,26 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css'
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
-class Movie extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired,
-
-    }
-
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} title={this.props.title}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+function Movie({ poster, title }) {
+  return (
+    <div>
+      <MoviePoster poster={poster} title={title}/>
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
-class MoviePoster extends Component {
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-    render() {
-        
-        return(
-            <img src={this.props.poster} alt={this.props.title}/>
-        );
-    }
+function MoviePoster({ title, poster }) {
+  return <img src={poster} alt={title} />;
 }
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
+MoviePoster.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
 
 export default Movie;
