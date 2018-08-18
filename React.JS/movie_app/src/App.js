e6a39c60cbc2604772ca2bfd53d15b0d46475690
hth225 <hth225@naver.com>
2018-08-18 22:31:13 +0900

    Promises

--- React.JS/movie_app/src/App.js
+++ React.JS/movie_app/src/App.js
@@ -7,47 +7,22 @@ class App extends Component {
   // Component LifeCycle
   // render : componentWillMount() -> render() -> componentDidMount()
   // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster:
-              "https://images.amcnetworks.com/ifc.com/wp-content/uploads/2016/09/The-Matrix-Revolutions-Hugo-Weaving-600x400.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster:
-              "http://images.mentalfloss.com/sites/default/files/styles/mf_image_16x9/public/full-metal-jacket-hed.jpg?itok=z83XGyRg&resize=1100x1100"
-          },
-          {
-            title: "OldBoy",
-            poster:
-              "http://www.gablescinema.com/media/filmassets/slides/oldboy_5.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster:
-              "https://lumiere-a.akamaihd.net/v1/images/swbfii_keyart_clean_final_flat_8d03e069.jpeg?region=0%2C69%2C1024%2C512&width=480"
-          },
-          {
-            title: "Transporter 2",
-            poster:
-              "https://i.pinimg.com/originals/c6/09/0f/c6090f29a3ce4b0b013522c85cca8704.jpg"
-          }
-        ]
-      });
-    }, 2000);
+    // promise는 첫 번째 작업이 끝나지 않아도 두 번째 작업을 수행함
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(err => console.log(err));
   }
 
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
-      return (<Movie title={movie.title} poster={movie.poster} key={index} />);
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
     });
-    return (movies);
+    return movies;
   };
 
   render() {
