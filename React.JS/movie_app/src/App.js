573314a6bb09cb9008103ef05881c26497407d7f
hth225 <hth225@naver.com>
2018-08-18 23:21:54 +0900

    Async and await

--- React.JS/movie_app/src/App.js
+++ React.JS/movie_app/src/App.js
@@ -11,19 +11,30 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    // promise는 첫 번째 작업이 끝나지 않아도 두 번째 작업을 수행함
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(err => console.log(err));
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />;
     });
     return movies;
   };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    // promise는 첫 번째 작업이 끝나지 않아도 두 번째 작업을 수행함
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
