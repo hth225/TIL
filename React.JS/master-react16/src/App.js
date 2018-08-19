099aed0774d44ed82e707a865bebc482adf4be8d
hth225 <hth225@naver.com>
2018-08-19 15:43:28 +0900

    can return string and Fragment

--- React.JS/master-react16/src/App.js
+++ React.JS/master-react16/src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
+
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          
-      </div>
+      <Fragment>
+        <ReturnTypes />
+      </Fragment>
     );
   }
 }
